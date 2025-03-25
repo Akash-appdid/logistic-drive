d40348c835b0d6187d6ee5242d86cd63915ea6ac
Mitsinh-Rajput <mitsinh.appdid@gmail.comgit config --list>
2025-03-25 11:31:03 +0530

    Refactor CustomDatePicker to include today parameter and datePickerMode, and update theme colors

--- lib/views/base/date_picker_widget.dart
+++ lib/views/base/date_picker_widget.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../../services/theme.dart';
+
 class CustomDatePicker extends StatelessWidget {
   const CustomDatePicker({
-    Key? key,
+    super.key,
     required this.child,
+    required this.today,
     required this.onChanged,
     this.getTime = false,
     this.years = 0,
     this.enabled,
-  }) : super(key: key);
+    this.datePickerMode = DatePickerMode.day,
+  });
 
   final Widget child;
+  final bool today;
   final bool getTime;
   final bool? enabled;
   final int years;
+  final DatePickerMode datePickerMode;
   final Function(DateTime? dateTime) onChanged;
 
   @override
@@ -23,8 +29,9 @@ class CustomDatePicker extends StatelessWidget {
         if (enabled ?? true) {
           showDatePicker(
             context: context,
+            initialDatePickerMode: datePickerMode,
             initialDate: DateTime.now().subtract(Duration(days: 356 * years)),
-            firstDate: DateTime(1940),
+            firstDate: today ? DateTime.now() : DateTime(1940),
             lastDate: years == 0 ? DateTime.now().add(const Duration(days: 356 * 10)) : DateTime.now().subtract(Duration(days: 356 * years)),
             builder: (context, child) {
               return Theme(
@@ -35,13 +42,13 @@ class CustomDatePicker extends StatelessWidget {
                         ),
                   ),
                   colorScheme: ColorScheme.light(
-                    primary: Theme.of(context).primaryColor, // header background color
+                    primary: primaryColor, // header background color
                     onPrimary: Colors.white, // header text color
-                    onSurface: Theme.of(context).primaryColor, // body text color
+                    onSurface: primaryColor, // body text color
                   ),
                   textButtonTheme: TextButtonThemeData(
                     style: TextButton.styleFrom(
-                      foregroundColor: Theme.of(context).primaryColor,
+                      foregroundColor: primaryColor,
                     ),
                   ),
                 ),
@@ -57,18 +64,18 @@ class CustomDatePicker extends StatelessWidget {
                   return Theme(
                     data: Theme.of(context).copyWith(
                       textTheme: TextTheme(
-                        headlineMedium: Theme.of(context).textTheme.titleMedium?.copyWith(
-                              fontSize: 20.0,
-                            ),
+                        headlineMedium: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 20.0, color: Colors.black87),
                       ),
                       colorScheme: ColorScheme.light(
-                        primary: Theme.of(context).primaryColor, // header background color
+                        secondary: primaryColor,
+
+                        primary: primaryColor, // header background color
                         onPrimary: Colors.white, // header text color
-                        onSurface: Theme.of(context).primaryColor, // body text color
+                        onSurface: primaryColor, // body text color
                       ),
                       textButtonTheme: TextButtonThemeData(
                         style: TextButton.styleFrom(
-                          foregroundColor: Theme.of(context).primaryColor,
+                          foregroundColor: primaryColor,
                         ),
                       ),
                     ),
