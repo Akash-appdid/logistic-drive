766645d9dae0fd858fb1365afda78c352d66e5f9
Mitsinh-Rajput <mitsinh.appdid@gmail.comgit config --list>
2025-03-25 17:16:23 +0530

    Add new asset paths for images and SVGs to enhance UI components

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -3,6 +3,7 @@ class Assets {
   Assets._();
 
   static const String images2Wheeler = 'assets/images/2-wheeler.png';
+  static const String imagesArrowRight = 'assets/images/arrow-right.png';
   static const String imagesBodyPackTruck = 'assets/images/body-pack-truck.png';
   static const String imagesDeliveryBike = 'assets/images/delivery_bike.png';
   static const String imagesDeliveryTruck = 'assets/images/delivery_truck.png';
@@ -14,8 +15,17 @@ class Assets {
   static const String imagesPlaceholder = 'assets/images/placeholder.jpg';
   static const String imagesProfileUnderReview = 'assets/images/profile_under_review.png';
   static const String imagesShimmer = 'assets/images/shimmer.gif';
+  static const String svgsAboutUs = 'assets/svgs/about-us.svg';
   static const String svgsBike = 'assets/svgs/bike.svg';
+  static const String svgsBookings = 'assets/svgs/bookings.svg';
+  static const String svgsEdit = 'assets/svgs/edit.svg';
+  static const String svgsHelpSupport = 'assets/svgs/help-support.svg';
+  static const String svgsMyProfile = 'assets/svgs/my-profile.svg';
   static const String svgsOtpverficationlockicon = 'assets/svgs/otpverficationlockicon.svg';
+  static const String svgsPrivacyPolicy = 'assets/svgs/privacy-policy.svg';
   static const String svgsProfile = 'assets/svgs/profile.svg';
+  static const String svgsReviewRating = 'assets/svgs/review-rating.svg';
+  static const String svgsSupport = 'assets/svgs/support.svg';
+  static const String svgsTermsAndCondition = 'assets/svgs/terms-and-condition.svg';
   static const String svgsTruck = 'assets/svgs/truck.svg';
 }
